d93ffde562587fea6cdc2b6a7ae138fc89e6ac3f
Brandon Dees <brandon@rietta.com>
2017-01-18 20:11:46 -0600

    some refactoring to support user-supplied layout file... lacks security for now

--- app/controllers/kitchen_sink/sinks_controller.rb
+++ app/controllers/kitchen_sink/sinks_controller.rb
@@ -2,6 +2,30 @@ require_dependency 'kitchen_sink/application_controller'
 
 module KitchenSink
   class SinksController < ApplicationController
-    def sink; render layout: 'application'; end
+    def sink
+      render layout: appropriate_layout
+    end
+
+    private
+
+    def appropriate_layout
+      return sanitized_layout_file_param if layout_file_param
+      default_layout_file
+    end
+
+    def layout_file_param
+      params[:layout_file]
+    end
+
+    def sanitized_layout_file_param
+      # TODO: use a whitelist mechanism of some kind
+      # possibly pre-load the known layout files' names
+      # from the directory and restrict to one of those
+      layout_file_param
+    end
+
+    def default_layout_file
+      'application'.freeze
+    end
   end
 end
